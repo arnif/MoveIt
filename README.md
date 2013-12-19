a6fb3441a08f5f407a0c7461943bf4a50909d6d7
arnif <arnifa@gmail.com>
2013-12-19 19:09:14 +0000

    inital commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ MoveIt
 ======
 
 Python script to move files based on the json file to correct folders
+
+How to:
+Add the name of TvShow to the json file with correct path
+Run the script from your Ready folder
+
+The script will generate regex based on TV show name
---
+++ moveit.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import re
+import os
+import json
+import shutil
+
+
+json_data = open('locations.json')
+data = json.load(json_data)
+path = '.'
+files = os.listdir(path)
+
+
+def main():
+	count = 0
+	for i in data['tvshows']:
+		show = data['tvshows'][count]['name']
+		goHere = data['tvshows'][count]['location']
+		reggie = createRegex(show)
+
+		# matcha dotid vid eitthvad i files arrayinu
+		for foo in files:
+			matchObj = re.search( reggie, foo, re.IGNORECASE)
+			if matchObj:
+				fileName = reggie + ".*(.mp4|.mkv|.avi)"
+				matchFile = re.search(fileName, foo, re.IGNORECASE)
+
+				if os.path.isdir(foo):
+					# get the file from the folder and remove it
+					print "FOLDER " + foo
+					os.chdir(foo)
+					isItHere = os.listdir(".")
+					for l in isItHere:
+						matchObj = re.search(fileName, l, re.IGNORECASE)
+						if matchObj:
+							print l + " goes " + goHere
+							shutil.move(l, goHere)
+
+				else:
+					if matchFile:
+						# move that shit (foo to goHere)
+						print foo + " goes " + goHere
+						shutil.move(foo, goHere)
+				
+				
+		count = count + 1
+
+
+def createRegex(show):
+
+	arr = show.replace(show[:1],"^.")
+	arr = arr.split(" ")
+	first = arr[0] + ".*"
+	# fileEnd = ".*(.mp4|.mkv|.avi)"
+	fileEnd = ".*."
+
+	#replace rest first char with ".*."
+	newArr = []
+	for i in arr:
+		newString  = i.replace(i[:1], ".*.")
+		newArr.append(newString)
+
+	newArr[0] = first
+	newArr.append(fileEnd)
+	returnThis = "".join(newArr)
+
+	return returnThis
+
+
+main()
+json_data.close()
